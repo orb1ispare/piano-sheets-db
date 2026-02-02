2148dfabeb01bc71e352a25f40d4b0a4bf4b230f
orb1ispare <254788828+orb1ispare@users.noreply.github.com>
2026-02-02 16:42:37 +0300

    Update favorites for user 918031124

--- users/data.js
+++ users/data.js
@@ -1,9 +1,9 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-02-02 13:42:27 UTC
+// Updated: 2026-02-02 13:42:36 UTC
 
 export const favorites = {
-  "918031124": ["another-love", "grandmas-home"],
+  "918031124": ["another-love"],
   "53462190": ["another-love"],
   "919705100": ["kid-cudi", "married-life", "be-nice-2-me"],
   "915931217": ["golden-hour"],
