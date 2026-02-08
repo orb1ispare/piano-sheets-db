802615fd3b65ff978a45f78dd741bd3a19f85a1a
orb1ispare <254788828+orb1ispare@users.noreply.github.com>
2026-02-09 01:44:56 +0300

    Update favorites for user 43305368

--- users/data.js
+++ users/data.js
@@ -1,6 +1,6 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-02-08 22:44:27 UTC
+// Updated: 2026-02-08 22:44:55 UTC
 
 export const favorites = {
   "918031124": ["another-love"],
@@ -119,6 +119,6 @@ export const favorites = {
   "18122909": ["sinking-town"],
   "581354021": ["in-the-sea", "jane-doe", "back-to-friends"],
   "265472972": ["the-real-slim-shady"],
-  "43305368": ["after-like", "stick-season", "sad-song-we-the-kings", "pork-soda", "promise", "ditto", "aria-math", "bling-bang-bang-born", "unravel", "f1", "locked-away", "lost-woods", "love-story-indila"],
+  "43305368": ["after-like", "stick-season", "sad-song-we-the-kings", "pork-soda", "promise", "ditto", "aria-math", "bling-bang-bang-born", "unravel", "f1", "locked-away", "lost-woods", "love-story-indila", "love-in-the-dark"],
   "912890230": ["coffin-dance", "jingle-bells", "love-story-taylor-swift", "rick-roll", "carol-of-the-bells", "tetris-theme", "kimetsu-no-yaiba-op", "somewhere-only-we-know"]
 };
