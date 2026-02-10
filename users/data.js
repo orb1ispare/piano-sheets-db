0d5a007eb9c3431de40a10a8e95530a47b63e8a6
orb1ispare <254788828+orb1ispare@users.noreply.github.com>
2026-02-10 20:49:47 +0300

    Update favorites for user 297145978

--- users/data.js
+++ users/data.js
@@ -1,6 +1,6 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-02-10 03:47:55 UTC
+// Updated: 2026-02-10 17:49:46 UTC
 
 export const favorites = {
   "918031124": ["another-love"],
@@ -122,5 +122,6 @@ export const favorites = {
   "43305368": ["after-like", "stick-season", "sad-song-we-the-kings", "pork-soda", "promise", "ditto", "bling-bang-bang-born", "unravel", "f1", "locked-away", "lost-woods", "love-story-indila", "aria-math", "love-in-the-dark"],
   "912890230": ["coffin-dance", "jingle-bells", "carol-of-the-bells", "tetris-theme", "kimetsu-no-yaiba-op", "somewhere-only-we-know"],
   "713227559": ["like-him", "love-like-you", "hype-boy", "stick-season"],
-  "954388718": ["be-nice-2-me"]
+  "954388718": ["be-nice-2-me"],
+  "297145978": ["like-him"]
 };
