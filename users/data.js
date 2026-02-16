3e55af0a4bccbd6d556d191283b7b14aeb22fe08
orb1ispare <254788828+orb1ispare@users.noreply.github.com>
2026-02-16 10:16:40 +0300

    Update favorites for user 50543641

--- users/data.js
+++ users/data.js
@@ -1,6 +1,6 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-02-16 07:11:47 UTC
+// Updated: 2026-02-16 07:16:40 UTC
 
 export const favorites = {
   "918031124": ["another-love"],
@@ -139,5 +139,6 @@ export const favorites = {
   "966851410": ["snuff"],
   "620987275": ["the-star-spangled-banner"],
   "97199265": ["kid-cudi"],
-  "933015755": ["grandmas-home"]
+  "933015755": ["grandmas-home"],
+  "50543641": ["squid-game"]
 };
