9e8d088f07fc3880656db21db6cfce8be79cf008
orb1ispare <254788828+orb1ispare@users.noreply.github.com>
2026-02-21 01:07:34 +0300

    Update favorites for user 79251703

--- users/data.js
+++ users/data.js
@@ -1,6 +1,6 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-02-20 21:51:04 UTC
+// Updated: 2026-02-20 22:07:33 UTC
 
 export const favorites = {
   "816774921": ["its-been-so-long", "love-story-indila"],
@@ -8,5 +8,5 @@ export const favorites = {
   "36619352": ["erika"],
   "742785636": ["pork-soda", "all-i-want-for-christmas-is-you"],
   "43305368": ["all-girls-are-the-same", "stick-season", "pork-soda", "promise", "love-story-indila", "chest-pain-i-love", "if-you-love-these-people", "debito-di-sonno", "five-nights-at-freddys-movie", "his-theme"],
-  "79251703": ["coffin-dance", "another-love", "lunch"]
+  "79251703": ["coffin-dance", "another-love", "lunch", "izmir-marsi"]
 };
