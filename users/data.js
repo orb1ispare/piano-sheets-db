2e7531c73ebfa6887abdc1f8e742d096ceebbb12
orb1ispare <254788828+orb1ispare@users.noreply.github.com>
2026-02-20 21:44:28 +0300

    Update favorites for user 43305368

--- users/data.js
+++ users/data.js
@@ -1,11 +1,11 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-02-20 18:44:22 UTC
+// Updated: 2026-02-20 18:44:28 UTC
 
 export const favorites = {
   "816774921": ["its-been-so-long", "love-story-indila"],
   "451609118": ["love-story-indila"],
   "36619352": ["erika"],
   "742785636": ["pork-soda", "all-i-want-for-christmas-is-you"],
-  "43305368": ["all-girls-are-the-same", "stick-season", "pork-soda", "promise", "dearly-beloved", "love-in-the-dark"]
+  "43305368": ["all-girls-are-the-same", "stick-season", "pork-soda", "promise", "dearly-beloved", "love-in-the-dark", "if-you-love-these-people"]
 };
