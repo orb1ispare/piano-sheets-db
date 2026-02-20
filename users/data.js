3b4accb511eea2cf1c5553c8af2c1a347ec1aa8a
orb1ispare <254788828+orb1ispare@users.noreply.github.com>
2026-02-20 04:31:13 +0300

    Update favorites for user 816774921

--- users/data.js
+++ users/data.js
@@ -1,7 +1,7 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-02-20 01:31:10 UTC
+// Updated: 2026-02-20 01:31:13 UTC
 
 export const favorites = {
-  "816774921": ["five-nights-at-freddys-movie", "song-of-storms"]
+  "816774921": ["song-of-storms"]
 };
